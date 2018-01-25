35dfc024200a43928156d4cc5b7f1912eeef8682
Marcus Hultkrantz <marcus.hultkrantz@live.se>
2018-01-25 12:07:32 +0100

    Example method

--- sl-data-collector/sl-data-collector/Example.cs
+++ sl-data-collector/sl-data-collector/Example.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 using SocketIo;
 using SocketIo.SocketTypes;
 
@@ -7,5 +9,23 @@ namespace sl_data_collector
     public class Example
     {
         SocketIo.SocketIo Socket = Io.Create("127.0.0.1", 4533, 4533, SocketHandlerType.Tcp);
+
+        public void GetInformation()
+        {
+            string html = string.Empty;
+            string url = @"https://api.stackexchange.com/2.2/answers?order=desc&sort=activity&site=stackoverflow";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(html);
+        }
     }
 }
